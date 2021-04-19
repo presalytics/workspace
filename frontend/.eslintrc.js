5d45e69e3087a0cd8a9131895d246cb35aa2cbdf
Kevin Hannegan <kevin@presalytics.io>
2021-04-19 10:00:37 -0700

    rm tests, allow console statments

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   extends: 'vuetify',
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
   parserOptions: {
